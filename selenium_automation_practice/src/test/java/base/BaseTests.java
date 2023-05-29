02e9b2ed07773ad30a1fd307d4c4451b6146e464
RavishanBx <ravishana@spaceitlabs.com>
2023-05-29 13:01:38 +0530

    created tests

--- selenium_automation_practice/src/test/java/base/BaseTests.java
+++ selenium_automation_practice/src/test/java/base/BaseTests.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 
 import java.util.List;
@@ -12,6 +14,7 @@ public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
 
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
@@ -20,12 +23,9 @@ public class BaseTests {
         homePage = new HomePage(driver);
 
         System.out.println(driver.getTitle());
-
-        driver.quit();
     }
-
-    public static void main(String args[]){
-        BaseTests test = new BaseTests();
-        test.setUp();
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
     }
 }
